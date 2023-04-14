e74e6d0c17b9b394753ff2d5ba8aa84f7dc045fb
ytyaru <yttry0@gmail.com>
2023-04-14 21:06:54 +0900

    とりま

--- docs/1/js/size.js
+++ docs/1/js/size.js
@@ -22,10 +22,13 @@ class Size {
         })
         this.Mode = this.Modes.Horizontal
     }
+    get Size() { return Object.freeze(this.#deepCopy(this.#size)) }
+    #deepCopy(obj) { return JSON.parse(JSON.stringify(obj)); } // structuredClone()未実装のため
     set Target(v) { 
         this.#target = document.querySelector('main') || document.body
-    set WritingMode(v) {
-
+        WritingMode.Mode = v
+        if (WritingMode.isHozirontal()) { this.#setSizeHorizontal() }
+        else { this.#setSizeVertical() }
     }
     set ColumnCount(v) {
 
@@ -39,6 +42,59 @@ class Size {
     calc(writingMode, columnCount, lineOfChars) {
 
     }
+    #setSizeHorizontal() {
+        this.#size.screen.inline = screen.width;
+        this.#size.screen.block = screen.height;
+        this.#size.avail.inline = screen.availWidth;
+        this.#size.avail.block = screen.availHeight;
+        this.#size.outer.inline = window.outerWidth;
+        this.#size.outer.block = window.outerHeight;
+        this.#size.inner.inline = window.innerWidth;
+        this.#size.inner.block = window.innerHeight;
+        this.#size.client.inline = document.documentElement.clientWidth;
+        this.#size.client.block = document.documentElement.clientHeight;
+        this.#size.scrollbar.inline = this.#size.inner.inline - this.#size.client.inline;
+        this.#size.scrollbar.block = this.#size.inner.block - this.#size.client.block;
+        this.#size.scroll.inline = document.documentElement.scrollWidth
+        this.#size.scroll.start = document.documentElement.scrollLeft
+        if (this.#target) {
+            this.#size.target.client.inline = this.#target.clientWidth
+            this.#size.target.client.block = this.#target.clientHeight
+            this.#size.target.scroll.inline = this.#target.scrollWidth
+            this.#size.target.scroll.start = this.#target.scrollLeft
+        }
+        console.log(this.#size)
+        console.log(this.#target)
+    }
+    #setSizeVertical() {
+        this.#size.screen.inline = screen.height;
+        this.#size.screen.block = screen.width;
+        this.#size.avail.inline = screen.availHeight;
+        this.#size.avail.block = screen.availWidth;
+        this.#size.outer.inline = window.outerHeight;
+        this.#size.outer.block = window.outerWidth;
+        this.#size.inner.inline = window.innerWidth;
+        this.#size.inner.block = window.innerWidth;
+        this.#size.client.inline = document.documentElement.clientHeight;
+        this.#size.client.block = document.documentElement.clientWidth;
+        this.#size.scrollbar.inline = this.#size.inner.inline - this.#size.client.inline;
+        this.#size.scrollbar.block = this.#size.inner.block - this.#size.client.block;
+        this.#size.scroll.inline = document.documentElement.scrollHeight
+        this.#size.scroll.start = document.documentElement.scrollTop
+        if (this.#target) {
+            this.#size.target.client.inline = this.#target.clientHeight
+            this.#size.target.client.block = this.#target.clientWidth
+            this.#size.target.scroll.inline = this.#target.scrollHeight
+            this.#size.target.scroll.start = this.#target.scrollTop
+        }
+        console.log(this.#size)
+        console.log(this.#target)
+    }
+
+}
+class WiringModeArea {
+    constructor() { this.inline=0, this.block=0, this.start=0, this.end=0 }
+    constructor() { [this.inline, this.block, this.start, this.end] = [0,0,0,0] }
 }
 window.Size = new Size()
 })()
