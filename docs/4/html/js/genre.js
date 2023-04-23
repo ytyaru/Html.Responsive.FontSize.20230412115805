56a4ab22b9aecf93d4f9a3d0ded8545376b620ab
ytyaru <yttry0@gmail.com>
2023-04-23 16:34:07 +0900

    work example 1

--- docs/4/html/js/genre.js
+++ docs/4/html/js/genre.js
@@ -1,7 +1,12 @@
 (function() {
 class Genre {
-    constructor() { this.genres = null; this.genreSelect = null; this.subGenreSelect = null; }
+    constructor() { this.genres = null; this.workExamples = new Map(); this.genreSelect = null; this.subGenreSelect = null; }
+    //async load() { await Promise.all([this.loadGenres(), this.loadWorkExamples()]); }
     async load() {
+        await this.loadGenres()
+        await this.loadWorkExamples()
+    }
+    async loadGenres() {
         if (this.genres) { return this.genres }
         this.genres = await Tsv.load(`locales/en/genre.tsv`)
         console.log(this.genres)
@@ -9,7 +14,28 @@ class Genre {
             this.genres[i].triple = this.genres[i].triple.split(',')
             this.genres[i].sub = this.genres[i].sub.split(',')
         }
-        return this.genres
+        //return this.genres
+    }
+    async loadWorkExample(subGenreId) {
+        if (!this.workExamples.has(subGenreId)) {
+            const path = `locales/en/genre-works/${genre.sid}/${sub}.tsv`
+            this.workExamples.set(sub, await Tsv.load(path))
+        }
+        return this.workExamples.get(subGenreId)
+    }
+    async loadWorkExamples() {
+        for (let genre of this.genres) {
+            for (let sub of genre.sub) {
+                const path = `locales/en/genre-works/${genre.sid}/${sub}.tsv`
+                this.workExamples.set(sub, await Tsv.load(path))
+            }
+        }
+        console.log(this.workExamples)
+        /* TSVファイル先頭にヘッダを一括追記した
+            TARGET=/tmp/work/Html.Responsive.FontSize.20230412115805/docs/4/html/locales/en/genre-works/
+            cd "$TARGET"
+            find . -name '*.tsv' | xargs sed -i '1s/^/nameEn\turlEn\tnameJa\turlJa\n/'
+        */
     }
     async makeUi() {
         if (!this.genres) { await this.load() }
@@ -35,6 +61,10 @@ class Genre {
             document.getElementById('genre-icon').src = `asset/image/icon/genre/png/220x220/${genre.lid}.png`
             document.getElementById('genre-icon').title = genre.label
         })
+        this.subGenreSelect = document.getElementById('sub-genre')
+        this.subGenreSelect.addEventListener('change', (e) => {
+            this.#resetWorkExamples(e.target.value)
+        })
     }
     #resetSubGenreSelect(genreId) {
         if (this.genres.some(g=>g.sid===genreId)) {
@@ -48,6 +78,11 @@ class Genre {
                 this.subGenreSelect.appendChild(option)
             }
         }
+    }
+    #resetWorkExamples(subGenreId) {
+        console.log(this.workExamples.get(subGenreId))
+//        `locales/en/genre-works/wd/cop-whydunit.tsv`
+//        `locales/en/genre-works/${this.genreSelect.value}/${subGenreId}.tsv`
     }
     /*
     makeGenreSelect() {
